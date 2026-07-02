2eea4c18a5075617acef33a6a9dda37dd8144e79
RickyChan21 <rickychan.21@gmail.com>
2026-07-01 23:54:58 -0500

    Remove unused static mount and imports

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import os
 import json
-from datetime import datetime
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from sqlmodel import Session, create_engine, select
 from dotenv import load_dotenv
@@ -18,8 +16,6 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 
 templates = Jinja2Templates(directory="templates")
-
-app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/", response_class=HTMLResponse)
 async def dashboard(request: Request):
